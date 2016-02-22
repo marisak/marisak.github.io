e61bbc2d752e85759a71248be88eb40c83985578
Marisa Kanemoto <mkanemoto@ucsd.edu>
2016-02-21 19:58:15 -0800

    using lab 7 as a placeholder sorry everyone

---
+++ public/js/introHCI.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("button.likeBtn").click(likeClick);
+}
+
+function likeClick(e) {
+	ga('send', 'event', 'like', 'click');
+}
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+
+
+var projects = require('../projects.json');
+
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num >= 0) {
+		projects['grid'] = false;
+		res.render('index', projects);
+	} else {
+		res.redirect('/grid');
+	}
+};
+
+exports.viewGrid = function(req, res) {
+	projects['grid'] = true;
+	res.render('index', projects);
+}
+
+
